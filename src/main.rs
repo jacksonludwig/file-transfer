f7f82faa864cea9c126851581a4653d1bb0a8671
Jackson <jacksonludwig0@gmail.com>
2020-07-12 12:15:15 -0400

    try serde

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::str;
 
 const REQUEST_AMOUNT: usize = 100;
 const THREAD_AMOUNT: usize = 8;
+const BUFFER_SIZE: usize = 4096;
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
@@ -22,7 +23,7 @@ fn main() -> std::io::Result<()> {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    let mut buffer = vec![0; 4096];
+    let mut buffer = vec![0; BUFFER_SIZE];
     let bytes_read = stream.read(&mut buffer).unwrap();
     println!("bytes read: {}", bytes_read);
     println!(
